5b7a2412ff849993216c3fae8edfc0ab959178c1
AnishaBeck <75293907+AnishaBeck@users.noreply.github.com>
2022-05-10 11:41:21 +0530

    update

---
+++ kanban/nodeapp.js
@@ -0,0 +1,46 @@
+const mysql=require('mysql2')
+const express=require('express')
+const port =8000;
+const app=express();
+const path=require('path');
+const bodyParser=require('body-parser');
+const cors=require('cors');
+app.use(cors({
+    orgin: "*",
+}))
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(__dirname+'/public'));
+
+ const connection=mysql.createConnection(
+     {
+         host:'localhost',
+         user:'root',
+         password:'4009',
+         database:'pmt'
+     }
+ )
+connection.connect((err)=>
+{
+    if(!err)
+    console.log('Database connection eshtablished....!')
+    else
+    console.log('Error')
+})
+
+app.listen(port,()=>
+{
+    console.log('Connection Eshtablished successfully....!!!')
+}
+)
+
+app.get('/test1', (req, res) => {
+    connection.query('select * from activity', (err, data) => {
+        if(!err){
+            return res.json(data)
+        }
+        else{
+            return res.sendStatus(500).end()
+        }
+    })
+})
