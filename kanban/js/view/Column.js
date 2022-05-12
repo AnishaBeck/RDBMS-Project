4e58d04e76d618ecb8e926431f89e41b8e710168
AnishaBeck <75293907+AnishaBeck@users.noreply.github.com>
2022-05-12 15:37:05 +0530

    kanban

--- kanban/js/view/Column.js
+++ kanban/js/view/Column.js
@@ -12,13 +12,26 @@ export default class Column {
 		this.elements.items = this.elements.root.querySelector(".kanban__column-items");
 		this.elements.addItem = this.elements.root.querySelector(".kanban__add-item");
 
-		this.elements.root.dataset.id = id;
+		// add a variable with sql queiry and pass that instead of id:
+		// id = id=req.params.id;
+		// app.get('/view',(req,res)=>
+        // {
+        //     var sql = "SELECT * from activity WHERE task_id="+id;
+        //     //console.log(id);
+        //     connection.query(sql,(err,rows,fields)=>
+        //     {
+        //         if(!err)
+        //             res.send(rows);
+        //         else
+        //             console.log('Error in Displaying')
+        // })
+		// })
+		this.elements.root.dataset.id = id; //this id here |^
 		this.elements.title.textContent = title;
 		this.elements.items.appendChild(topDropZone);
 
 		this.elements.addItem.addEventListener("click", () => {
 			const newItem = KanbanAPI.insertItem(id, "");
-
 			this.renderItem(newItem);
 		});
 
@@ -46,4 +59,40 @@ export default class Column {
 
 		this.elements.items.appendChild(item.elements.root);
 	}
+
+}
+
+//loading data from json to a table
+async function loadIntoTable(url, table) {
+	const tableHead = table.querySelector("thead");
+	const tableBody = table.querySelector("tbody");
+	const response = await fetch(url);
+	const {header, rows} = await response.json();
+
+	//clear the table
+	tableHead.innerHTML = "<tr></tr>";
+	tableBody.innerHTML = "";
+
+	//populate the header
+	for (const headerText of headers){
+		const headerElement = document.createElement("th");
+
+		headerElement.textContent = headerText;
+		tableHead.querySelector("tr").appendChild(headerElement);
+	}
+
+	//populate the rows
+	for (const row of rows){
+		const rowElement = document.createElement("td");
+
+		for (const cellText of row){
+			const cellElement = document.createElement("td");
+
+			cellElement.textContent = cellText;
+			rowElement.appendChild(cellElement);
+		}
+
+		tableBody.appendChild(rowElement);
+	}
+	loadIntoTable("./data.json", document.querySelector("table"));
 }
